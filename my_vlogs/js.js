f8ff4ef32ec82064a248fbb35ad485abaf0ecbe2
Curtisbib04 <e.abdykaimova@yandex.com>
2019-03-05 17:32:52 +0600

    Добавила переключатель темы

--- my_vlogs/js.js
+++ my_vlogs/js.js
@@ -1,7 +1,35 @@
-function backGround() {
-	document.getElementById('this').className = "baclGround";
+function click(e){
+	if (e.target.value == 1) {
+		// alert("Светлая тема выбрана!");
+		document.getElementById('navBar').className = "navbar navbar-light bg-light";
+
+
+		document.getElementById('this').className = "baclGroundLight colorDark";
+		document.getElementById('id_H2_1').className = "baclGroundLight colorDark";
+		document.getElementById('id_H2_2').className = "baclGroundLight colorDark";
+		document.getElementById('id_H2_3').className = "baclGroundLight colorDark";
+
+		document.getElementById('id_Div_1').className = "baclGroundLight colorDark";
+		document.getElementById('id_Div_2').className = "baclGroundLight colorDark";
+		document.getElementById('id_Div_3').className = "baclGroundLight colorDark";
+
+	}else if(e.target.value == 2) {
+		// alert("Темная тема выбрана!");
+		document.getElementById('navBar').className = "navbar navbar-dark bg-dark";
+
+		document.getElementById('this').className = "baclGroundDark colorLight";
+		document.getElementById('id_H2_1').className = "baclGroundDark colorLight";
+		document.getElementById('id_H2_2').className = "baclGroundDark colorLight";
+		document.getElementById('id_H2_3').className = "baclGroundDark colorLight";
+
+		document.getElementById('id_Div_1').className = "baclGroundDark colorLight";
+		document.getElementById('id_Div_2').className = "baclGroundDark colorLight";
+		document.getElementById('id_Div_3').className = "baclGroundDark colorLight";
+	}
+    
+    var enabled = e.target.value;
 }
 
-function cicle() {
-
+for (var i = 0; i < myForm.enabled.length; i++) {
+    myForm.enabled[i].addEventListener("click", click);
 }
